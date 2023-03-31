5e652fe649777e12d7b3013f1bf10b73837d4678
Toleyorbek <128204542+Toleyorbek@users.noreply.github.com>
2023-03-31 09:08:55 +0500

    Task1

--- homework3/homework3/Program.cs
+++ homework3/homework3/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("son kiriting ");
+            int a = int.Parse(Console.ReadLine());
+            int b, c = 0;
+            for (int i = 1; i <= a; i++)
+            {
+                if (a % i == 0)
+                {
+                    c++;
+                }
+            }
+            if (c == 2)
+            {
+                Console.WriteLine("tub son");
+            }
+            else
+            {
+                Console.WriteLine("tub emas");
+            };
         }
     }
 }
